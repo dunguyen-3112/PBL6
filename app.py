b8946587578a0868ac404f74e39634bc4c00cf5a
dunguyen-3112 <dunguyenhuu.it2019@gmail.com>
2022-11-11 01:36:10 +0700

    clear

--- app.py
+++ app.py
@@ -4,10 +4,11 @@ from PIL import Image
 from matplotlib import pyplot as plt
 import time
 
-# thiết lập tiêu đề 
+# thiết lập tiêu đề
 st.title("Distracted Driver Detection")
 
 fig = plt.figure()
+
 
 def main():
     # lấy path hình ảnh được chọn
@@ -18,7 +19,7 @@ def main():
     if file_uploaded is not None:
         image = Image.open(file_uploaded)
         st.image(image, caption='Uploaded Image', use_column_width=True)
-        st.write('image',image)
+        st.write('image', image)
 
     if class_btn:
         if file_uploaded is None:
@@ -27,7 +28,7 @@ def main():
             with st.spinner('Model working....'):
                 plt.imshow(image)
                 plt.axis("off")
-                st.write('image',image)
+                st.write('image', image)
                 predictions = return_prediction(image)
                 time.sleep(1)
                 st.success('Classified')
